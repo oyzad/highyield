24d227389c5941ae1c80e2d6777f1bc09f43e4cd
oyzad <oyzad5b7@gmail.com>
2025-05-27 09:50:07 +0100

    Added Shop Items
    Added new Shop items, with added functionality e.g scaled prices

--- game.js
+++ game.js
@@ -1,19 +1,44 @@
+// variables 
+
 let buds = 0;
 let cursors = 0;
+let cursorPrice = 10;
+let lights = 0;
+let lightPrice = 50;
+let clickstrength = 1;
+
 
 const budDisplay = document.getElementById('bud-count');
 const cursorDisplay = document.getElementById('cursor-count');
+const lightDisplay = document.getElementById('light-count');
 
-document.getElementById('cookie').addEventListener('click', () => {
-  buds++;
+document.getElementById("bud").addEventListener('click', () => {
+  buds += clickstrength
   updateDisplay();
 });
 
+function updateShop(){
+  document.getElementById("buy-cursor").innerText = `Buy Cursor (${cursorPrice} bud)`;
+  document.getElementById("buy-light").innerText = `Buy Light (${lightPrice} bud)`;
+}
+
 function buyCursor() {
-  if (buds >= 10) {
-    buds -= 10;
+  if (buds >= cursorPrice) {
+    buds -= cursorPrice;
     cursors += 1;
+    cursorPrice = Math.floor(cursorPrice * 1.15);
+    updateShop();
     updateDisplay();
+  }
+}
+function buyLight(){
+  if (buds >= lightPrice) {
+    buds -= lightPrice;
+    lights += 1;
+    clickstrength += 1
+    lightPrice = Math.floor(lightPrice * 1.15);
+    updateShop();
+    updateDisplay()
   }
 }
 
@@ -23,6 +48,12 @@ setInterval(() => {
 }, 1000);
 
 function updateDisplay() {
-  budDisplay.textContent = cookies;
+  budDisplay.textContent = buds;
   cursorDisplay.textContent = cursors;
+  lightDisplay.textContent = lights;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  updateShop(); // Run after DOM is ready
+});
+
