da97ceef599715fc48cc2d297c806e25df113b07
oyzad <oyzad5b7@gmail.com>
2025-05-27 14:33:22 +0100

    Added Stats and Background

--- game.js
+++ game.js
@@ -8,10 +8,15 @@ let strainPrice = 500;
 let lights = 0;
 let lightPrice = 50;
 let clickstrength = 1;
+let previousBudCount = 0;
+let calculatedBPS = 0;
+
+
 
 const strains = ["Sour Diesel","Purple Urkle","Blue Dream","Northern Lights"]
 const strainFile = ["Bud_SD.png", "Bud_PU.png", "Bud_BB.png", "Bud_NL.png"]
-const strainPrices = [500,750,1000,1500,2000,5000]
+const strainPrices = [500,2000,5000,7500,10000,50000]
+const strainClick = [0,10,15,20,25,40]
 let budFile = "bud_SD.png"; 
 
 const budDisplay = document.getElementById('bud-count');
@@ -57,9 +62,10 @@ function buyLight(){
     buySound.play();
     buds -= lightPrice;
     lights += 1;
-    clickstrength += 1
+    clickstrength += 1;
     lightPrice = Math.floor(lightPrice * 1.15);
     updateShop();
+    updateStats();
     updateDisplay();
   }
 }
@@ -69,6 +75,8 @@ function upgradeStrain(){
     strainSound.play();
     buds -= strainPrice;
     strainNum += 1;
+    clickstrength += strainClick[strainNum];
+    updateTooltips();
     updateShop();
     updateBudImage();
     updateDisplay();
@@ -76,23 +84,48 @@ function upgradeStrain(){
   }
 }
 
-
 setInterval(() => {
   buds += cursors;
   updateDisplay();
 }, 1000);
+
+function updateStats() {
+  document.getElementById("buds-per-click").textContent = clickstrength;
+  document.getElementById("auto-harvest").textContent = cursors;
+}
 
 function updateDisplay() {
   budDisplay.textContent = buds;
   cursorDisplay.textContent = cursors;
   lightDisplay.textContent = lights;
   strainDisplay.textContent = strains[strainNum];
+  updateStats();
+  updateTooltips();
 }
 function updateBudImage () {
   const budImage = document.getElementById("bud");
   budFile = strainFile[strainNum]
   budImage.src = `assets/${budFile}`;
 }
+
+setInterval(() => {
+  const budDiff = buds - previousBudCount;
+  calculatedBPS = budDiff;
+  previousBudCount = buds;
+  document.getElementById("buds-per-second").textContent = calculatedBPS;
+}, 1000);
+
+function updateTooltips() {
+
+
+  // Strain tooltip
+  document.getElementById("strain-tooltip").setAttribute(
+    "data-tooltip",
+    `Upgrades to a new strain, increases strain click bonus. Current strain bonus: ${strainClick[strainNum+1]}`
+  );
+}
+
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
